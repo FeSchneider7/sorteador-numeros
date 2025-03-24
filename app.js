050a95d79c9ce413abe15c9945d491545e982230
Fernando Schneider <fernandomoraes26@gmail.com>
2025-03-24 18:05:12 -0300

    aula

---
+++ app.js
@@ -0,0 +1,10 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`quantidade:  ${quantidade} números`);
+    alert(`de:  ${de} `);
+    alert(`ate:  ${ate} `);
+    
+}
