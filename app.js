01c6a8b6cee2abf026c37cd34f34ec6aa2f73ea8
Fernando Schneider <fernandomoraes26@gmail.com>
2025-03-26 16:26:41 -0300

    aula

--- app.js
+++ app.js
@@ -8,15 +8,50 @@ function sortear() {
 
     for (let i = 0; i < quantidade; i++){
         numeroSorteado = obterNumeroAleatorio(de, ate);
+        while (numerosSorteados.includes(numeroSorteado)){
+            numeroSorteado = obterNumeroAleatorio(de, ate);
+        }
         numerosSorteados.push(numeroSorteado);
 
     }
 
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${numerosSorteados}</label>`;
+
+    alterarStatusBotao(); 
+
+    
+
+
     // let numeroSorteado = obterNumeroAleatorio(de, ate);
-   alert(numerosSorteados);
+   // alert(numerosSorteados);
  
 }
 
 function obterNumeroAleatorio(de, ate) {
     return Math.floor(Math.random() * (ate - de + 1)) + de;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    
+    if (botao.classList.contains('container__botao-desabilitado')) {
+
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+ 
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+
+    alterarStatusBotao();
+
 }
